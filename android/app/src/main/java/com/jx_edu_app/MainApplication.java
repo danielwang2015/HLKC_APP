ccb70d50bfea3c228a447e50e40426041e2265a8
Danielwang2015 <dong_wang2008@163.com>
2017-08-30 11:17:37 +0800

    first version

--- android/app/src/main/java/com/jx_edu_app/MainApplication.java
+++ android/app/src/main/java/com/jx_edu_app/MainApplication.java
@@ -3,9 +3,7 @@ package com.jx_edu_app;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnative.photoview.PhotoViewPackage;
-import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,9 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
-            new PhotoViewPackage(),
-            new ImagePickerPackage()
+            new PhotoViewPackage()
       );
     }
   };
